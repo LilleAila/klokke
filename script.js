a5d93496b5a2d8bfac54605c9eb84438cac67299
Lille Aila <67327023+LilleAila@users.noreply.github.com>
2020-10-10 18:11:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var d = {
+    date: new Date(),
+    seconds: new Date().getSeconds(),
+    minutes: new Date().getMinutes(),
+    hours: new Date().getHours()
+}
+// console.log(d.seconds);
+if (d.hours >= 12) {
+    d['hours'] -= 12;
+}
+d['seconds']++;
+d['minutes']++;
+d['hours']++;
+
+function setNewDate() {
+    d = {
+        date: new Date(),
+        seconds: new Date().getSeconds(),
+        minutes: new Date().getMinutes(),
+        hours: new Date().getHours()
+    }
+    if (d.hours >= 12) {
+        d['hours'] -= 12;
+    }
+    d['seconds']++;
+    d['minutes']++;
+    // d['hours']++;
+    // console.log(d.hours);
+    // console.log(d.seconds);
+    setTime(d);
+}
+
+function setTime(time) {
+    $('.hours').css('transform', `translate(-50%, 50%) rotate(${time.hours * 30 + time.minutes / 60 * 30}deg)`);
+    $('.minutes').css('transform', `translate(-50%, 50%) rotate(${time.minutes * 6}deg)`);
+    // $('.minutes').css('transform', `translate(-50%, 50%) rotate(${11 * 30}deg)`);
+    $('.seconds').css('transform', `translate(-50%, 50%) rotate(${time.seconds * 6}deg)`);
+}
+
+setInterval(() => setNewDate(), 100);
