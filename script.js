9c44b0e5f450049bd6aaf699c37e6cf90eed6a95
Lille Aila <67327023+LilleAila@users.noreply.github.com>
2020-10-11 08:22:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function setNewDate() {
     d = {
         date: new Date(),
         seconds: new Date().getSeconds(),
+        milliseconds: new Date().getMilliseconds(),
         minutes: new Date().getMinutes(),
         hours: new Date().getHours()
     }
@@ -34,7 +35,7 @@ function setTime(time) {
     $('.hours').css('transform', `translate(-50%, 50%) rotate(${time.hours * 30 + time.minutes / 60 * 30}deg)`);
     $('.minutes').css('transform', `translate(-50%, 50%) rotate(${time.minutes * 6}deg)`);
     // $('.minutes').css('transform', `translate(-50%, 50%) rotate(${11 * 30}deg)`);
-    $('.seconds').css('transform', `translate(-50%, 50%) rotate(${time.seconds * 6}deg)`);
+    $('.seconds').css('transform', `translate(-50%, 50%) rotate(${time.seconds * 6 + time.milliseconds * 0.006}deg)`);
 }
 
-setInterval(() => setNewDate(), 100);
+setInterval(() => setNewDate(), 1);
